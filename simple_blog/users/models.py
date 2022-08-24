f75377b2c6c9e068f50896ae558e37c1740e94e0
Benjy <benjy444@gmail.com>
2022-08-24 00:37:14 -0300

    Funciona

---
+++ simple_blog/users/models.py
@@ -0,0 +1,31 @@
+"""Users models."""
+#Aquí se define la estructura de la base de datos en lo que se refiere a usuarios
+
+# Django
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Profile(models.Model):
+    """Profile model.
+
+    Proxy model that extends the base data with other
+    information.
+    """
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+
+    website = models.URLField(max_length=200, blank=True)
+
+    photo = models.ImageField(
+        upload_to='users/pictures',
+        blank=True,
+        null=True
+    )
+
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        """Return username."""
+        return self.user.username
+
